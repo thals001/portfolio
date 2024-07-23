24864803ce7a57730907127e3f315994500a55ae
thals001 <thals062172@gmail.com>
2024-07-23 17:13:11 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -106,9 +106,9 @@
 	var pieChart = function() {
 		$('.chart').easyPieChart({
 			scaleColor: false,
-			lineWidth: 4,
+			lineWidth: 5,
 			lineCap: 'butt',
-			barColor: '#FF9000',
+			barColor: '#7e84d0',
 			trackColor:	"#f5f5f5",
 			size: 160,
 			animate: 1000
